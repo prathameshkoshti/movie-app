a81867a16c558f271224577277f21a48887ac002
Prathamesh Koshti <prathamesh@Prathameshs-MacBook-Air.local>
2020-03-03 22:03:22 +0530

    Build added

---
+++ build/precache-manifest.df46258fe62c281945d4909558f99e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfe899137a40fb723d494eb67184dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e33d36cde47444ee69",
+    "url": "/static/css/main.b9cf8959.chunk.css"
+  },
+  {
+    "revision": "4eac7eaf5759ea7b3666",
+    "url": "/static/js/2.b054fb34.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b054fb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e33d36cde47444ee69",
+    "url": "/static/js/main.bf38a9c5.chunk.js"
+  },
+  {
+    "revision": "d7a6c9176e9e28c3ff53",
+    "url": "/static/js/runtime-main.47a1d944.js"
+  }
+]);
